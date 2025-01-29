9c6c68025f8cfb4e5d8913b11782ae0e80b44599
yoni-crypto <yonidisu111@gmail.com>
2025-01-30 01:45:54 +0300

    added some additional websites

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -6,57 +6,71 @@ import CanvasLoader from '../components/CanvasLoader'
 import DemoComputer from '../components/DemoComputer'
 
 const Projects = () => {
-    const projectsCount=myProjects.length
-    const [selectedProjectIndex,setSelectedProjectIndex]=useState(0)
-    const currentProject=myProjects[selectedProjectIndex]
-    const handleNavigation=(direction)=>{
-        setSelectedProjectIndex((prevIndex)=>{
-            if(direction==='previous'){
-                return prevIndex===0?projectsCount-1:prevIndex-1
-            }else{
-                return prevIndex==projectsCount-1?0:prevIndex+1
+    const projectsCount = myProjects.length
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(0)
+    const currentProject = myProjects[selectedProjectIndex]
+    const handleNavigation = (direction) => {
+        setSelectedProjectIndex((prevIndex) => {
+            if (direction === 'previous') {
+                return prevIndex === 0 ? projectsCount - 1 : prevIndex - 1
+            } else {
+                return prevIndex == projectsCount - 1 ? 0 : prevIndex + 1
             }
         })
     }
-  return (
-    <section id='#work' className='c-space my-20'>
-        <p className='head-text'>My work</p>
-        <div className=' mt-12  w-full'>
-            <div className='flex flex-col gap-5 relative sm:p-10 px-5 shadow-2xl shadow-black-200'>
-                <div className='absolute top-0 right-0'>
-                    <img src={currentProject.spotlight} alt='spotlight' className='w-full h-96 object-cover rounded-xl'/>
+
+    const navtohref = () => {
+        window.location.href = currentProject.href
+    }
+    return (
+        <section id='#work' className='c-space my-20'>
+            <p className='head-text'>My work</p>
+            <div className='mt-12 w-full'>
+                <div className='flex flex-col gap-5 relative sm:p-10 px-5 shadow-2xl shadow-black-200'>
+                    <div className='absolute top-0 right-0'>
+                        <img src={currentProject.spotlight} alt='spotlight' className='w-full h-96 object-cover rounded-xl' />
+                    </div>
+                    <div className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg' style={currentProject.logoStyle}>
+                        <img src={currentProject.logo} alt='logo' className='w-10 h-10 shadow-sm' />
+                    </div>
+                    <div className='flex flex-col gap-5 text-white-600 my-5'>
+                        <p className='text-white text-2xl font-semibold animatedText'>{currentProject.title}</p>
+                        <p className='animatedtext'>{currentProject.desc}</p>
+                        <p className='animatedtext'>{currentProject.subdesc}</p>
+                    </div>
+                    <div className='flex items-center justify-beween flex-wrap gap-5'>
+                        <div className='flex items-center gap-3'>
+                            {currentProject.tags.map((tag, index) => (
+                                <div className='tech-logo' key={index}>
+                                    <img src={tag.path} alt={tag.name} />
+                                </div>
+                            ))}
+                        </div>
+                        {/* Container for the "Check live site" link */}
+                        <div className="">
+                            <a
+                                className="flex items-center gap-2 cursor-pointer text-white-600"
+                                href={currentProject.href}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <p className="text-white">Check live site</p>
+                                <img src="/assets/arrow-up.png" className="w-3 h-3" alt="arrow" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className='flex justify-between items-center mt-7'>
+                        <button className='arrow-btn' onClick={() => handleNavigation('previous')}>
+                            <img src="/assets/left-arrow.png" alt="left arrow" className='w-4 h-4' />
+                        </button>
+                        <button className='arrow-btn' onClick={() => handleNavigation('next')}>
+                            <img src="/assets/right-arrow.png" alt="right arrow" className='w-4 h-4' />
+                        </button>
+                    </div>
                 </div>
-                <div className='p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg' style={currentProject.logoStyle}>
-                    <img src={currentProject.logo} alt='logo' className='w-10 h-10 shadow-sm'/>
-                </div>
-                <div className='flex flex-col gap-5 text-white-600 my-5'>
-                    <p className='text-white text-2xl font-semibold animatedText '>{currentProject.title}</p>
-                    <p className='animatedtext'>{currentProject.desc}</p>
-                    <p className='animatedtext'>{currentProject.subdesc}</p>
-                </div>
-                <div className='flex items-center justify-between flex-wrap gap-5'>
-                    <div className='flex items-center gap-3'>
-                        {currentProject.tags.map((tag,index)=>(
-                            <div className='tech-logo' key={index}>
-                                <img src={tag.path} alt={tag.name}/>
-                            </div>
-                        ))} 
-                </div>
-                <a className='flex items-center gap-2 cursor-pointer text-white-600' href={currentProject.href} target='_blank' rel='noreferrer'>
-                    <p>Check live site</p>
-                    <img src='/assets/arrow-up.png' className='w-3 h-3 ' alt='arrow'/>
-                </a>
-                </div>
-                <div className='flex justify-between items-center mt-7'>
-                    <button className='arrow-btn' onClick={()=>handleNavigation('previous')}>
-                        <img src="/assets/left-arrow.png" alt="left arrow" className='w-4 h-4' />
-                    </button>
-                    <button className='arrow-btn' onClick={()=>handleNavigation('next')}>
-                        <img src="/assets/right-arrow.png" alt="right arrow" className='w-4 h-4' />
-                    </button>
-                </div>
-            </div>
-            {/* <div className='border border-black-300 bg-black-200 rounded-lg h-96 md:h-full '>
+
+
+                {/* <div className='border border-black-300 bg-black-200 rounded-lg h-96 md:h-full '>
                 <Canvas>
                     <ambientLight intensity={1}/>
                     <directionalLight position={[10,10,5]}/>
@@ -69,10 +83,10 @@ const Projects = () => {
                     </Center>
                 </Canvas>
             </div> */}
-        </div>
-      
-    </section>
-  )
+            </div>
+
+        </section>
+    )
 }
 
 export default Projects
